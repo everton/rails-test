0fa67040569c52d525a132baa8fa070a7e7cab50
Gram <graham@beecrazy.com>
2012-11-02 03:32:13 +0800

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BcTest::Application.config.secret_token = 'b7a3510a3bb8432ab0676ac220636c47ed7d376c3af47704cee05d9772a44052049dcf6c6f14ae1cf62b35b97e5170d0d89fcea90a580f6335f70863e4230d77'
